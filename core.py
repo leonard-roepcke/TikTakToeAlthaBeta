530a4ab69e79a064d19744a3ca154ee31dfae1b3
leonard-roepcke <leonard.roepcke.2@gmail.com>
2026-01-28 14:07:23 +0100

    ui fix, display of player x oder o

--- core.py
+++ core.py
@@ -2,4 +2,3 @@ from gameState import GameState
 class Core:
     def __init__(self, gameState:GameState):
         self.gameState:GameState = gameState
-        print(self.gameState)
--- ui.py
+++ ui.py
@@ -14,7 +14,8 @@ class Ui:
             if i < 2:
                 print("---+---+---")
     def get_input(gameState: GameState):
-        player = sum(gameState.data) * -1
+        player = sum([cell for row in gameState.data for cell in row]) * -1
         if not player: player = 1
         player_char = "X" if player == 1 else "O"
         print("player: ", player_char, " ist dran.")
+
