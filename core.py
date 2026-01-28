4f32422c2ebf955240b73b1a0bc7c20bf6b07f8a
leonard-roepcke <leonard.roepcke.2@gmail.com>
2026-01-28 11:36:18 +0100

    simple struckture

---
+++ core.py
@@ -0,0 +1,5 @@
+from gameState import GameState
+class Core:
+    def __init__(self, gameState:GameState):
+        self.gameState:GameState = gameState
+        print(self.gameState)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("hello world")
+from core import Core
+from gameState import GameState
+
+Core(GameState())
